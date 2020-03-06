ce80e5403c1a4d5f4d360b09d78928d4f8263ad9
Ali107 <Alialawadhi2014bh@gmail.com>
2020-03-06 17:12:19 +0300

    Made it so that the framerate caps at 1000 fps instead of 60 fps.

--- Engine/Graphics.cpp
+++ Engine/Graphics.cpp
@@ -49,8 +49,8 @@ Graphics::Graphics( HWNDKey& key )
 	sd.BufferDesc.Width = Graphics::ScreenWidth;
 	sd.BufferDesc.Height = Graphics::ScreenHeight;
 	sd.BufferDesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
-	sd.BufferDesc.RefreshRate.Numerator = 1;
-	sd.BufferDesc.RefreshRate.Denominator = 60;
+	sd.BufferDesc.RefreshRate.Numerator = 1000;
+	sd.BufferDesc.RefreshRate.Denominator = 1;
 	sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
 	sd.OutputWindow = key.hWnd;
 	sd.SampleDesc.Count = 1;
