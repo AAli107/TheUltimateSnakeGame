9772cc3cb90f915b826b0a7508559ff0ca42287c
Ali107 <Alialawadhi2014bh@gmail.com>
2020-03-09 21:24:47 +0300

    Made Game Over text to be red.

--- Engine/CellCodex.h
+++ Engine/CellCodex.h
@@ -11,6 +11,7 @@ public:
 	void DrawMainMenu();
 private:
 	Board& brd;
+	Color gameOverTextColor = Colors::MakeRGB(255, 0, 0);
 	Color textColor = Colors::MakeRGB(150, 200, 0);
 	Color menuTextColor = Colors::MakeRGB(255, 255, 255);
 };
